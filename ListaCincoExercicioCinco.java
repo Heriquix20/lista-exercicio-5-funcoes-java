d3888d1b29c1e66b5c4eda116e618829dd5124b9
Henrique Cezar <129986380+Heriquix20@users.noreply.github.com>
2023-05-26 13:08:39 -0300

    Add files via upload

---
+++ ListaCincoExercicioCinco.java
@@ -0,0 +1,27 @@
+package ListaCincoExercicios;
+import java.util.Scanner;
+
+public class ListaCincoExercicioCinco {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nFunção para imprimir número informado pelo usuário");
+
+        int numero;
+
+        System.out.println("\nDigite um número: ");
+        numero = scanner.nextInt();
+
+        imprimirNumeroInformadoUsuário(numero);
+        scanner.close();
+    }
+
+    public static void imprimirNumeroInformadoUsuário(int numero) {
+        for (int i = 1; i <= numero; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
